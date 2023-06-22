3ce3207f6baa3125836350dffe8a0d746174e372
Alexander Dinauer <alexander.dinauer@sentry.io>
2023-06-22 16:42:41 +0200

    make CI happier

--- src/mapper.rs
+++ src/mapper.rs
@@ -54,14 +54,12 @@ impl<'m> Iterator for RemappedFrameIter<'m> {
             }
             // parents of inlined frames don’t have an `endline`, and
             // the top inlined frame need to be correctly offset.
-            let line = if member.original_endline.is_none() {
+            let line = if member.original_endline.is_none()
+                || member.original_endline.unwrap() == member.original_startline
+            {
                 member.original_startline
             } else {
-                if member.original_endline.unwrap() == member.original_startline {
-                    member.original_startline
-                } else {
-                    member.original_startline + frame.line - member.startline
-                }
+                member.original_startline + frame.line - member.startline
             };
             // when an inlined function is from a foreign class, we
             // don’t know the file it is defined in.

c206ebb385cb45344f97917cd0bcc2fb6e90f0bb
Sebastian Zivota <loewenheim@mailbox.org>
2024-07-01 21:31:48 +0200

    Improve debug impls and add display

--- src/cache/debug.rs
+++ src/cache/debug.rs
@@ -1,7 +1,5 @@
 use std::fmt;
 
-use watto::StringTable;
-
 use crate::ProguardCache;
 
 use super::raw;
@@ -13,28 +11,42 @@ pub struct ClassDebug<'a, 'data> {
     pub(crate) raw: raw::Class,
 }
 
+impl ClassDebug<'_, '_> {
+    fn obfuscated_name(&self) -> &str {
+        self.cache
+            .read_string(self.raw.obfuscated_name_offset)
+            .unwrap()
+    }
+
+    fn original_name(&self) -> &str {
+        self.cache
+            .read_string(self.raw.original_name_offset)
+            .unwrap()
+    }
+
+    fn file_name(&self) -> Option<&str> {
+        self.cache.read_string(self.raw.file_name_offset).ok()
+    }
+}
+
 impl fmt::Debug for ClassDebug<'_, '_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Class")
-            .field(
-                "obfuscated_name",
-                &StringTable::read(
-                    self.cache.string_bytes,
-                    self.raw.obfuscated_name_offset as usize,
-                ),
-            )
-            .field(
-                "original_name",
-                &StringTable::read(
-                    self.cache.string_bytes,
-                    self.raw.original_name_offset as usize,
-                ),
-            )
-            .field(
-                "file_name",
-                &StringTable::read(self.cache.string_bytes, self.raw.file_name_offset as usize),
-            )
+            .field("obfuscated_name", &self.obfuscated_name())
+            .field("original_name", &self.original_name())
+            .field("file_name", &self.file_name())
             .finish()
+    }
+}
+
+impl fmt::Display for ClassDebug<'_, '_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} -> {}:", self.original_name(), self.obfuscated_name())?;
+        if let Some(file_name) = self.file_name() {
+            writeln!(f)?;
+            write!(f, r##"# {{"id":"sourceFile","fileName":"{file_name}"}}"##)?;
+        }
+        Ok(())
     }
 }
 
@@ -45,47 +57,110 @@ pub struct MemberDebug<'a, 'data> {
     pub(crate) raw: raw::Member,
 }
 
+impl MemberDebug<'_, '_> {
+    fn original_class(&self) -> Option<&str> {
+        self.cache.read_string(self.raw.original_class_offset).ok()
+    }
+
+    fn original_file(&self) -> Option<&str> {
+        self.cache.read_string(self.raw.original_file_offset).ok()
+    }
+
+    fn params(&self) -> &str {
+        self.cache
+            .read_string(self.raw.params_offset)
+            .unwrap_or_default()
+    }
+
+    fn obfuscated_name(&self) -> &str {
+        self.cache
+            .read_string(self.raw.obfuscated_name_offset)
+            .unwrap()
+    }
+
+    fn original_name(&self) -> &str {
+        self.cache
+            .read_string(self.raw.original_name_offset)
+            .unwrap()
+    }
+
+    fn original_endline(&self) -> Option<u32> {
+        if self.raw.original_endline != u32::MAX {
+            Some(self.raw.original_endline)
+        } else {
+            None
+        }
+    }
+}
+
 impl fmt::Debug for MemberDebug<'_, '_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Member")
-            .field(
-                "obfuscated_name",
-                &StringTable::read(
-                    self.cache.string_bytes,
-                    self.raw.obfuscated_name_offset as usize,
-                )
-                .unwrap(),
-            )
+            .field("obfuscated_name", &self.obfuscated_name())
             .field("startline", &self.raw.startline)
             .field("endline", &self.raw.endline)
-            .field(
-                "original_class",
-                &StringTable::read(
-                    self.cache.string_bytes,
-                    self.raw.original_class_offset as usize,
-                ),
-            )
-            .field(
-                "original_file",
-                &StringTable::read(
-                    self.cache.string_bytes,
-                    self.raw.original_file_offset as usize,
-                ),
-            )
-            .field(
-                "original_name",
-                &StringTable::read(
-                    self.cache.string_bytes,
-                    self.raw.original_name_offset as usize,
-                ),
-            )
+            .field("original_name", &self.original_name())
+            .field("original_class", &self.original_class())
+            .field("original_file", &self.original_file())
             .field("original_startline", &self.raw.original_startline)
-            .field("original_endline", &self.raw.original_endline)
-            .field(
-                "params",
-                &StringTable::read(self.cache.string_bytes, self.raw.params_offset as usize),
-            )
+            .field("original_endline", &self.original_endline())
+            .field("params", &self.params())
             .finish()
+    }
+}
+
+impl fmt::Display for MemberDebug<'_, '_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "    {}:{}:<ret> ", self.raw.startline, self.raw.endline)?;
+        if let Some(original_class) = self.original_class() {
+            write!(f, "{original_class}.")?;
+        }
+        write!(
+            f,
+            "{}({}):{}",
+            self.original_name(),
+            self.params(),
+            self.raw.original_startline
+        )?;
+        if let Some(end) = self.original_endline() {
+            write!(f, ":{end}")?;
+        }
+        write!(f, " -> {}", self.obfuscated_name())?;
+        Ok(())
+    }
+}
+
+pub struct CacheDebug<'a, 'data> {
+    cache: &'a ProguardCache<'data>,
+}
+
+impl fmt::Display for CacheDebug<'_, '_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for class in self.cache.classes {
+            writeln!(
+                f,
+                "{}",
+                ClassDebug {
+                    raw: class.clone(),
+                    cache: self.cache
+                }
+            )?;
+            let Some(members) = self.cache.get_class_members(class) else {
+                continue;
+            };
+
+            for member in members {
+                writeln!(
+                    f,
+                    "{}",
+                    MemberDebug {
+                        raw: member.clone(),
+                        cache: self.cache
+                    }
+                )?;
+            }
+        }
+        Ok(())
     }
 }
 
@@ -112,5 +187,12 @@ impl<'data> ProguardCache<'data> {
             cache: self,
             raw: m.clone(),
         })
+    }
+
+    /// Creates a view of the cache that implements `Display`.
+    ///
+    /// The `Display` impl is very similar to the original proguard format.
+    pub fn display(&self) -> CacheDebug {
+        CacheDebug { cache: self }
     }
 }
